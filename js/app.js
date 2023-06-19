5040162030512bf1032fc4c8a6a7756195cef0fc
Anrii <andrejsalogub3@gmail.com>
2023-06-19 15:49:10 +0300

    added functionality for the account of the final results when entering data

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const billInput = document.querySelector(".calculation__bill-input");
+const numberOfPeopleInput = document.querySelector(
+    ".calculation__number-of-people-input"
+);
+const tipsBtns = document.querySelectorAll(".calculation__tip-list-item");
+const totalPerPerson = document.querySelector(".amout__tip-total");
+const totalTipPerPerson = document.querySelector(".amout__tip-total-amount");
+let tip;
+
+const calcTotal = () => {
+    if (!+billInput.value || !+numberOfPeopleInput.value || !+tip) {
+        totalPerPerson.textContent = "$0.00";
+        totalTipPerPerson.textContent = "$0.00";
+        return;
+    }
+
+    totalPerPerson.textContent = `\$${(
+        (+billInput.value - +billInput.value * (+tip / 100)) /
+        +numberOfPeopleInput.value
+    ).toFixed(2)}`;
+
+    totalTipPerPerson.textContent = `\$${(
+        (+billInput.value * (+tip / 100)) /
+        +numberOfPeopleInput.value
+    ).toFixed(2)}`;
+};
+
+calcTotal();
+
+billInput.addEventListener("input", () => {
+    calcTotal();
+});
+
+numberOfPeopleInput.addEventListener("input", () => {
+    calcTotal();
+});
+
+tipsBtns.forEach((tipBtn) => {
+    tipBtn.addEventListener("click", () => {
+        tip = tipBtn.getAttribute("data-percent");
+        calcTotal();
+    });
+});
